1459f248e397e3260b27d9adee91e28e0aabbcf6
zjasmin <zjasmin.pro@gmail.com>
2026-06-11 17:22:49 +0530

    missed

--- fix_creds.py
+++ fix_creds.py
@@ -1,6 +1,9 @@
-﻿from pymongo import MongoClient
-c = MongoClient('mongodb://mongo:27017/')
-db = c['mirador-vms']['cameras']
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'onvif-backend')))
+from app.core.database import db as _db
+
+db = _db['cameras']
 db.update_one({'ip': '192.168.126.235'}, {'$set': {'username': 'service', 'password': 'Admin123!', 'manufacturer': 'Bosch'}})
 db.update_one({'ip': '192.168.126.234'}, {'$set': {'username': 'service', 'password': 'Admin1234', 'manufacturer': 'Axis'}})
 db.update_one({'ip': '192.168.126.240'}, {'$set': {'username': 'MIRADOR VMS', 'password': 'Miradorvms@axis', 'manufacturer': 'Axis'}})
